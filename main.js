a61fb13b264d7c0a9cd031a7f6734e83428ec5f8
TeamBinaryBeast <64690821+TeamBinaryBeast@users.noreply.github.com>
2021-03-26 00:24:28 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,123 @@
+function calculate(button){
+    var screen = document.getElementById("screen");
+    var temp = document.getElementById('temp');
+    var sign = document.getElementById('sign');
+    var id  = button.id;
+    var total = 0
+    var numbutton = ['%', '.', "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    if(numbutton.indexOf(id) > -1 && !(screen.value.includes("%"))){
+        if((screen.value[0] == "0" && screen.value[1] != ".") && id != "."){
+            screen.value = "";
+            // sign.value = ""
+        }
+
+        if(sign.value == 'erase') {
+            screen.value = ""
+            sign.value = "";
+        }
+        
+        if(screen.value.includes(".") && id != "."){
+            screen.value += id;
+        }
+
+        if(!screen.value.includes(".")){
+            if((id == "." || id == "%") && (screen.value == "" || screen.value == "0")){
+                screen.value = "0";
+            }
+            screen.value += id;
+        }
+
+    }
+
+    if(id == "AC"){
+        screen.value = "0";
+        temp.value = "";
+        sign.value = ""
+    }
+
+    if(id == "C"){
+        var ln = screen.value.length;
+        screen.value = screen.value.slice(0, ln-1)
+        if(screen.value == ""){
+            screen.value = "0"
+        }
+    }
+
+    if (id == "+" || id == "-" || id == "*" || id == "/"){
+        if(screen.value.includes("%")){
+            screen.value = screen.value.slice(0, -1);
+            screen.value = parseFloat(screen.value) / 100;
+        }
+        
+        if(temp.value.includes("%")){
+            temp.value = temp.value.slice(0, -1);
+            temp.value = parseFloat(temp.value) / 100;
+        }
+
+        if (sign.value == "" || sign.value == "erase"){
+            temp.value = screen.value;
+            
+        } else {
+            if(sign.value == "+"){
+                temp.value = parseFloat(temp.value) + parseFloat(screen.value);
+            }
+    
+            if(sign.value == "-"){
+                temp.value = parseFloat(temp.value) - parseFloat(screen.value);
+            }
+    
+            if(sign.value == "*"){
+                temp.value = parseFloat(temp.value) * parseFloat(screen.value);
+            }
+    
+            if(sign.value == "/"){
+                if(screen.value == "0"){
+                    temp.value = 0
+                } else {
+                    temp.value = parseFloat(temp.value) / parseFloat(screen.value);
+                }
+            }
+        }
+        screen.value = "0"
+        sign.value = id;
+    } 
+    
+
+    if (id == "=" && screen.value != ""){
+        if(screen.value.includes("%")){
+            screen.value = screen.value.slice(0, -1);
+            screen.value = parseFloat(screen.value) / 100;
+        }
+        if(temp.value.includes("%")){
+            temp.value = temp.value.slice(0, -1);
+            temp.value = parseFloat(temp.value) / 100;
+        }
+
+        if(sign.value == "+"){
+            total = parseFloat(temp.value) + parseFloat(screen.value);
+        }
+
+        if(sign.value == "-"){
+            total = parseFloat(temp.value) - parseFloat(screen.value);
+        }
+
+        if(sign.value == "*"){
+            total = parseFloat(temp.value) * parseFloat(screen.value);
+        }
+
+        if(sign.value == "/"){
+            if(screen.value == "0"){
+                total = 0;
+            } else {
+                total = parseFloat(temp.value) / parseFloat(screen.value);
+            }
+            
+        }
+
+        sign.value = "erase";
+        temp.value = "";
+        screen.value = total;
+    }
+        
+}
